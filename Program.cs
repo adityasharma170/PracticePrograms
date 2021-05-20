e511b2ebedfdfdee8f1b93e8c3700df5d020736a
adityasharma170 <84501285+adityasharma170@users.noreply.github.com>
2021-05-20 14:28:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter line : ");
+            String line = Console.ReadLine();
+            Console.WriteLine(line);
+            string toTotalCase=  line.ToTitleCase();
+            Console.WriteLine(toTotalCase);
+
+        }
+
+        
+    }
+}
---
+++ StringExtensionscs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public static class StringExtensions
+    {
+        public static string ToTitleCase(this string s)
+        {
+            var upperCase = s.ToUpper();
+            var words = upperCase.Split(' ');
+            var minorWords = new String[] {"ON", "IN", "AT", "OFF", "UP", "TO","OF", "AS", "BY",//prepositions
+                                   "THE", "A",  "AN",//articles
+                                   "AND", "BUT", "OR", "NOR","YET", "FOR", "SO"};//conjunctions
+           
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (minorWords.Contains(words[i]))
+                {
+                    words[i] = words[i].ToLower();
+                }
+                else
+                {
+                    words[i] = words[i].ToPascalCase();
+                }
+            }
+            return string.Join(" ", words);
+
+        }
+        public static string ToPascalCase(this string s)
+        {
+            return s.Substring(0, 1).ToUpper() + s.Substring(1).ToLower();
+        }
+    }
+}

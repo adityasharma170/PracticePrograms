2989493585d95a2b140b8611e648368e0b45febf
adityasharma170 <84501285+adityasharma170@users.noreply.github.com>
2021-05-20 16:20:22 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,39 @@
 ﻿using System;
-
-namespace ConsoleApp4
+using System.IO;
+using System.Collections.Generic;
+namespace ConsoleApp8
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter line : ");
-            String line = Console.ReadLine();
-            Console.WriteLine(line);
-            string toTotalCase=  line.ToTitleCase();
-            Console.WriteLine(toTotalCase);
 
+        static void DirectorySearch(string dir)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(dir))
+                    Console.WriteLine("  -" + "{0}", f[(f.LastIndexOf("\\") + 1)..]);
+
+                foreach (string d in Directory.GetDirectories(dir))
+                {
+                    Console.WriteLine("--" + "{0}", d[(d.LastIndexOf("\\") + 1)..]);
+                    DirectorySearch(d);
+                }
+                
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        
-    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter directory path");
+            string root = @"" + Console.ReadLine();
+            DirectorySearch(root);
+            Console.ReadKey();
+        }
+        }
+
+   
 }
